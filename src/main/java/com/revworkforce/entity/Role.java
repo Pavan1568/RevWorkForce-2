e534805b43a0afa36265059337ec1deff89d0464
Pavan Sai <pavansaiboga946@gmail.com>
2026-02-21 12:44:52 +0530

    Added role entity

---
+++ src/main/java/com/revworkforce/entity/Role.java
@@ -0,0 +1,19 @@
+package com.revworkforce.entity;
+
+import com.revworkforce.enums.RoleType;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(unique = true, nullable = false)
+    private RoleType name;
+
+    // getters & setters
+}
