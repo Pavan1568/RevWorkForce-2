ea1df39b0c5786399d58258c0ed04b59c7a3e27d
Pavan Sai <pavansaiboga946@gmail.com>
2026-02-21 14:54:52 +0530

    Added Holiday repository

---
+++ src/main/java/com/revworkforce/repository/HolidayRepository.java
@@ -0,0 +1,12 @@
+package com.revworkforce.repository;
+
+import com.revworkforce.entity.Holiday;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface HolidayRepository extends JpaRepository<Holiday, Long> {
+
+    List<Holiday> findByDateBetween(LocalDate start, LocalDate end);
+}
