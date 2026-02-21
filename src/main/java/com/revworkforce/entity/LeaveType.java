4e4912dad6fef86fa642341bb7ffd2624f9401e9
Pavan Sai <pavansaiboga946@gmail.com>
2026-02-21 14:46:53 +0530

    Added the leave type class

---
+++ src/main/java/com/revworkforce/entity/LeaveType.java
@@ -0,0 +1,19 @@
+package com.revworkforce.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "leave_types")
+public class LeaveType extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    // getters & setters
+}
