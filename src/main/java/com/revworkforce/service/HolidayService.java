6c7870d145cbe6042d56bcad8a250e3c8596eded
Pavan Sai <pavansaiboga946@gmail.com>
2026-02-22 12:45:21 +0530

    added the service implementation for the holiday

---
+++ src/main/java/com/revworkforce/service/HolidayService.java
@@ -0,0 +1,17 @@
+package com.revworkforce.service;
+
+import com.revworkforce.entity.Holiday;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface HolidayService {
+
+    Holiday createHoliday(Holiday holiday);
+
+    void deleteHoliday(Long id);
+
+    List<Holiday> getAllHolidays();
+
+    List<Holiday> getHolidaysByYear(int year);
+}
